98e44e7c8309f33027e29571ce5bb0d915140520
Dwaipayan Bhowmik <bhowmikdwaipayan505@gmail.com>
2024-09-26 01:17:42 +0530

    deploy frontend

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -41,11 +41,11 @@ import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 //import router from "../../../backend/src/routes/userRoutes";
-
+import server from "../environment";
 export const AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: "http://localhost:8000/api/v1/users"
+    baseURL: `${server}/api/v1/users`
 });
 
 export const AuthProvider = ({ children }) => {
